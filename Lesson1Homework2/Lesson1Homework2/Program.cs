c36e833dfb9d6d774aa49768fb3d32f9b7352e99
unknown <rostykvoznyak@gmail.com>
2020-10-27 19:09:05 +0200

    Lesson1Homework2

---
+++ Lesson1Homework2/Lesson1Homework2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson1Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653d;
+            Decimal e = 2.7182818284590452M;
+            Console.WriteLine($"{pi}");
+            Console.WriteLine($"{e}");
+        }
+    }
+}
